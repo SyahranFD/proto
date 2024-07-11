031869dd56196d10d85858e1064cf60ea04005b7
Fadzaa <fadza20@gmail.com>
2024-07-12 02:42:22 +0700

    feat: [FA] fetch all component in landing page

--- src/app/components/landing-components/feed-component.tsx
+++ src/app/components/landing-components/feed-component.tsx
@@ -1,41 +1,52 @@
 import ChipExpertise from "@/app/components/common-components/chip-expertise";
 import Image from "next/image";
+import {getFeedProject} from "@/app/lib/services/api/project";
 
-export default function FeedComponent() {
+export default async function FeedComponent() {
+
+    const dataFeed = await getFeedProject();
+
+    console.log("CHECK DATA FEED")
+    console.log(dataFeed[0])
     return (
         <div className="flex flex-col gap-[20px]  w-full">
             <h1 className="text-primary font-semibold text-2xl">Feed</h1>
             <div className="grid grid-cols-3 gap-10">
-                <div className="bg-white  shadow-md flex flex-col justify-between gap-[20px]">
-                    <Image src="/assets/image/neosantara-mockup.png" alt="Project Mockup" width={300} height={200} className="rounded-[8px] w-full"/>
-                    <div className="flex flex-col justify-between items-start p-[15px] gap-[20px]">
-                        <div className="flex flex-col">
-                            <h2 className="text-primary font-medium ">Proto - Project Collaboration Platform</h2>
-                            <h3 className="text-[#A9A9A9] text-sm">Software Development</h3>
-                        </div>
-
-                        <div className="flex gap-5">
-                            <ChipExpertise expertise="UI/UX"/>
-                            <ChipExpertise expertise="Flutter"/>
-                            <ChipExpertise expertise="Laravel"/>
-                        </div>
-
-                        <div className="flex items-center gap-2">
-                            <div className="flex">
-                                <Image src="/assets/image/user-avatar.png" alt="User Avatar" width={40} height={40}
-                                       className="rounded-full -ms-3"/>
-                                <Image src="/assets/image/user-avatar.png" alt="User Avatar" width={40} height={40}
-                                       className="rounded-full -ms-3"/>
-                                <Image src="/assets/image/user-avatar.png" alt="User Avatar" width={40} height={40}
-                                       className="rounded-full -ms-3"/>
+                {dataFeed.map((project, index) => (
+                    <div key={index} className="bg-white  shadow-md flex flex-col justify-between gap-[20px]">
+                        <img src={project.image} alt="Project Mockup" width={300} height={200}
+                               className="rounded-[8px] w-full h-[200px] object-cover"/>
+                        <div className="flex flex-col justify-between items-start p-[15px] gap-[20px]">
+                            <div className="flex flex-col">
+                                <h2 className="text-primary font-medium ">{project.title}</h2>
+                                <h3 className="text-[#A9A9A9] text-sm">{project.category}</h3>
                             </div>
 
-                            <h3 className="text-[#A9A9A9] text-sm font-light">4 Collaborators in this project</h3>
+                            <div className="flex gap-5">
+                                {project.skill.map((skill, index) => (
+                                    <div key={index}>
+                                        <ChipExpertise expertise={skill.name}/>
+                                    </div>
+                                ))}
+                            </div>
+
+                            <div className="flex items-center gap-2">
+                                <div className="flex">
+                                    {project.participant.map((participant, index) => (
+                                        <div key={index}>
+                                            {/*<Image src={participant.full_name} alt="User Avatar" width={40} height={40}*/}
+                                            {/*       className="rounded-full -ms-3"/>*/}
+                                        </div>
+                                    ))}
+                                </div>
+
+                                <h3 className="text-[#A9A9A9] text-sm font-light">{`${project.participant.length} Collaborators in this project`}</h3>
+                            </div>
+
                         </div>
 
                     </div>
-
-                </div>
+                ))}
             </div>
         </div>
     )
--- src/app/components/landing-components/your-project-component.tsx
+++ src/app/components/landing-components/your-project-component.tsx
@@ -1,39 +1,53 @@
 import Image from "next/image";
 import ChipExpertise from "@/app/components/common-components/chip-expertise";
+import {getCurrentProject} from "@/app/lib/services/api/project";
+import Link from "next/link";
 
 export default async function YourProjectComponent() {
+
+    const dataCurrentProject = await getCurrentProject();
+
     return (
      <div className="flex flex-col gap-[20px]  w-full">
          <h1 className="text-primary font-semibold text-2xl">Your Project</h1>
-         <div className="bg-white p-[15px] shadow-md flex flex-col gap-[20px]">
+         {dataCurrentProject.map((project, index) => (
+             <div key={index} className="bg-white p-[15px] shadow-md flex flex-col gap-[20px]">
 
-             <div className="flex justify-between items-center">
-                 <div className="flex flex-col">
-                     <h2 className="text-primary font-medium ">Proto - Project Collaboration Platform</h2>
-                     <h3 className="text-[#A9A9A9] text-sm">Software Development</h3>
+                 <div className="flex justify-between items-center">
+                     <div className="flex flex-col">
+                         <h2 className="text-primary font-medium ">{project.title}</h2>
+                         <h3 className="text-[#A9A9A9] text-sm">{project.category}</h3>
+                     </div>
+
+                     <div className="flex">
+                         {project.participant.map((participant, index) => (
+                             <div key={index}>
+                                 <Image src="/assets/image/user-avatar.png" alt="User Avatar" width={40} height={40}
+                                        className="rounded-full -ms-3"/>
+                             </div>
+                         ))}
+                     </div>
+
+
                  </div>
 
-                 <div className="flex">
-                     <Image src="/assets/image/user-avatar.png" alt="User Avatar" width={40} height={40} className="rounded-full -ms-3" />
-                     <Image src="/assets/image/user-avatar.png" alt="User Avatar" width={40} height={40} className="rounded-full -ms-3" />
-                     <Image src="/assets/image/user-avatar.png" alt="User Avatar" width={40} height={40} className="rounded-full -ms-3" />
+                 <div className="flex justify-between items-center">
+                     <div className="flex gap-5">
+                         {project.skill.map((skill, index) => (
+                             <div key={index}>
+                                 <ChipExpertise expertise={skill.name}/>
+                             </div>
+                         ))}
+                     </div>
+
+                     <Link href={`project/${project.id}`}>
+                         <button className="bg-primary px-[18px] py-[5px] rounded-[16px]">
+                             <h3 className="font-semibold text-white text-sm">Continue</h3>
+                         </button>
+                     </Link>
                  </div>
-
-
              </div>
-
-             <div className="flex justify-between items-center">
-                 <div className="flex gap-5">
-                     <ChipExpertise expertise="UI/UX"/>
-                     <ChipExpertise expertise="Flutter"/>
-                     <ChipExpertise expertise="Laravel"/>
-                 </div>
-
-                 <button className="bg-primary px-[18px] py-[5px] rounded-[16px]">
-                     <h3 className="font-semibold text-white text-sm">Continue</h3>
-                 </button>
-             </div>
-         </div>
+         ))}
      </div>
     )
 }
