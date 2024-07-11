8164f2be42a4186129444d422a8088d40aa03fbe
Yasa Kafi Razzan <114861219+YasaKafi@users.noreply.github.com>
2024-07-12 00:22:28 +0700

    feat : [YK] add ui profile

--- src/app/(page)/(features-pages)/profile/page.tsx
+++ src/app/(page)/(features-pages)/profile/page.tsx
@@ -1,81 +1,143 @@
+import { Button } from "@/app/components/ui/button";
+import Image from "next/image";
+import React, { ReactNode } from "react";
+import { FaDribbble, FaGithub, FaLinkedin } from "react-icons/fa";
+import { FiPlus } from "react-icons/fi";
+import { MdEdit } from "react-icons/md";
+import {
+  Tabs,
+  TabsContent,
+  TabsList,
+  TabsTrigger,
+} from "@/app/components/ui/tabs";
+import { ComboboxDemo } from "@/app/components/ui/combobox";
 
+const ProfilePage: React.FC = () => {
 
-import Image from 'next/image';
-import React, { ReactNode } from 'react';
-import { FaDribbble, FaGithub, FaLinkedin } from 'react-icons/fa';
+  interface platformData {
+    id: number;
+    name: string;
+    icon: ReactNode;
+  }
 
-const ProfilePage:React.FC = () => {
+  interface progammingLanguageData {
+    id: number;
+    name: string;
+  }
 
-    interface platformData {
-        id: number;
-        name: string;
-        icon: ReactNode;
-      }
-      
-    interface progammingLanguageData {
-        id: number;
-        name: string;
-      }
-    
-      interface jobdeskData {
-        id: number;
-        name: string;
-      }
-    
-      const platforms: platformData[] = [
-        {
-          id: 1,
-          name: "Fattah Anggit Al Dzakwan",
-          icon: <FaLinkedin size={20} />,
-        },
-        {
-          id: 2,
-          name: "Fadza",
-          icon: <FaGithub size={20} />,
-        },
-        {
-          id: 3,
-          name: "Fattah Anggit",
-          icon: <FaDribbble size={20} />,
-        },
-      ];
-    
-      const progammingLanguages: progammingLanguageData[] = [
-        {
-          id: 1,
-          name: "Flutter",
-        },
-        {
-          id: 2,
-          name: "JawaScript",
-        },
-        {
-          id: 3,
-          name: "Laravel",
-        },
-        {
-          id: 4,
-          name: "Javva",
-        },
-      ];
-      const jobdesks: jobdeskData[] = [
-        {
-          id: 1,
-          name: "UI/UX Design",
-        },
-        {
-          id: 2,
-          name: "Frontend",
-        },
-        {
-          id: 3,
-          name: "Backend",
-        },
-      ];
- return (
-  <div className='w-full'>
-    <div className='w-full h-screen  flex flex-col pb-8'>
+  interface jobdeskData {
+    id: number;
+    name: string;
+  }
 
+  const platforms: platformData[] = [
+    {
+      id: 1,
+      name: "Fattah Anggit Al Dzakwan",
+      icon: <FaLinkedin size={24} />,
+    },
+    {
+      id: 2,
+      name: "Fadza",
+      icon: <FaGithub size={24} />,
+    },
+    {
+      id: 3,
+      name: "Fattah Anggit",
+      icon: <FaDribbble size={24} />,
+    },
+  ];
+
+  const progammingLanguages: progammingLanguageData[] = [
+    {
+      id: 1,
+      name: "Flutter",
+    },
+    {
+      id: 2,
+      name: "JawaScript",
+    },
+    {
+      id: 3,
+      name: "Laravel",
+    },
+    {
+      id: 4,
+      name: "Javva",
+    },
+  ];
+  const jobdesks: jobdeskData[] = [
+    {
+      id: 1,
+      name: "UI/UX Design",
+    },
+    {
+      id: 2,
+      name: "Frontend",
+    },
+    {
+      id: 3,
+      name: "Backend",
+    },
+  ];
+
+  const tabContents = [
+    {
+      id: 1,
+      value: "projectCollaboration",
+      title: "Noesantara - Indonesia Culture",
+      description: "Website",
+      imageSrc: "/assets/image/neosantara-mockup.png",
+      jobdesks: [
+        { id: 1, name: "UI/UX Design" },
+        { id: 2, name: "Frontend" },
+        { id: 3, name: "Backend" },
+      ],
+      collaborators: [
+        { id: 1, imageSrc: "/assets/image/user-avatar.png" },
+        { id: 2, imageSrc: "/assets/image/user-avatar.png" },
+        { id: 3, imageSrc: "/assets/image/user-avatar.png" },
+      ],
+      collaboratorCount: 4,
+      collaborationText: "4 Collaboration in this project",
+    },
+    {
+      id: 2,
+      value: "projectCollaboration",
+      title: "Noesantara - Indonesia Culture",
+      description: "Website",
+      imageSrc: "/assets/image/neosantara-mockup.png",
+      jobdesks: [
+        { id: 1, name: "UI/UX Design" },
+        { id: 2, name: "Frontend" },
+        { id: 3, name: "Backend" },
+      ],
+      collaborators: [
+        { id: 1, imageSrc: "/assets/image/user-avatar.png" },
+        { id: 2, imageSrc: "/assets/image/user-avatar.png" },
+        { id: 3, imageSrc: "/assets/image/user-avatar.png" },
+      ],
+      collaboratorCount: 4,
+      collaborationText: "4 Collaboration in this project",
+    },
+  ];
+
+  return (
+    <div className="w-full relative">
+      <div className=" absolute right-28 top-28">
+        <Image
+          className="rounded-full border-2 h-[280px] border-solid border-white shadow-xl"
+          src={"/assets/image/user-avatar.png"}
+          alt="profile picture"
+          width={280}
+          height={280}
+          style={{
+            objectFit: "cover",
+            color: "white",
+          }}
+        />
+      </div>
+      <div className="w-full h-screen  flex flex-col pb-8">
         <div className="w-full h-[20%] bg-red-100">
           <Image
             src={"/assets/image/banner-profile.png"}
@@ -89,31 +151,164 @@ const ProfilePage:React.FC = () => {
             }}
           />
         </div>
-        <div className='w-full h-[80%] flex'>
-            <div className='w-1/5 h-full  pt-12 pl-16'>
-            <h2 className="text-2xl font-semibold text-black">Portflio Platform</h2>
+        <div className="w-full h-[80%] flex">
+          <div className="w-1/5 h-full  pt-12 pl-16">
+            <h2 className="text-2xl font-semibold text-black">
+              Portflio Platform
+            </h2>
             {platforms.map((item: platformData, index: number) => (
               <div key={index} className="flex gap-2 mt-12">
                 {item.icon}
-                <h2 className="text-normal font-medium text-black">
-                  {item.name}
-                </h2>
+                <h2 className="text-lg font-medium text-black">{item.name}</h2>
               </div>
             ))}
+            <Button
+              variant="default"
+              size="sm"
+              className="w-auto flex gap-2 h-auto py-3 px-4 cursor-pointer mt-8 rounded-xl"
+            >
+              <FiPlus size={20} />
 
+              <h1 className="font-light text-normal ">
+                Add Portfolio Platforms
+              </h1>
+            </Button>
+          </div>
+
+          <div className="w-4/5 h-full  pt-12 pl-16">
+            <div className="w-full flex flex-col items-start">
+              <div className="flex gap-4 items-center mb-4">
+                <h2 className="text-4xl font-medium text-black">
+                  Fattah Anggit Al Dzakwan
+                </h2>
+                <MdEdit size={32} />
+              </div>
             </div>
-            
-            <div className='w-4/5 h-full  pt-12 pl-16'>
+            <h4 className="text-gray-400 text-[24px] flex justify-start gap-3">
+              Mobile Developer{" "}
+              <span className="text-gray-400 text-[24px] font-normal">
+                (18)
+              </span>
+            </h4>
+            <div className="flex flex-wrap gap-3">
+              {progammingLanguages.map(
+                (item: progammingLanguageData, index: number) => (
+                  <div
+                    key={index}
+                    className="mt-8 px-16 py-2 bg-blue-50 rounded-3xl"
+                  >
+                    <h2 className="text-lg font-medium text-blue-400">
+                      {item.name}
+                    </h2>
+                  </div>
+                )
+              )}
+            </div>
+            <div className="w-full mt-8 flex justify-between">
+              <Tabs defaultValue="account" className="w-[400px]">
+                <TabsList>
+                  <TabsTrigger
+                    style={{ fontSize: "28px" }}
+                    value="projectCollaboration"
+                  >
+                    Project Collaboration
+                  </TabsTrigger>
+                  <TabsTrigger
+                    style={{ fontSize: "28px" }}
+                    value="personalProject"
+                  >
+                    Personal Project
+                  </TabsTrigger>
+                </TabsList>
+                <div className="w-full flex gap-4 mt-4">
+                  {tabContents.map((tab, index) => (
+                    <TabsContent key={index} value={tab.value}>
+                      {tab.imageSrc && (
+                        <div className="w-[400px] shadow-xl rounded-xl">
+                          <Image
+                            src={tab.imageSrc}
+                            alt="project image"
+                            width={1000}
+                            height={1000}
+                            style={{
+                              width: "100%",
+                              height: "auto",
+                            }}
+                          />
+                          <div className="flex flex-col items-start p-4">
+                            <h2 className="text-2xl font-medium text-black mb-1">
+                              {tab.title}
+                            </h2>
+                            <h4 className="text-black text-lg">
+                              {tab.description}
+                            </h4>
+                            <div className="flex flex-wrap gap-3 mt-3">
+                              {tab.jobdesks.map((item, index) => (
+                                <div
+                                  key={index}
+                                  className="px-4 py-1 bg-blue-50 rounded-3xl"
+                                >
+                                  <h2 className="text-sm font-medium text-blue-400">
+                                    {item.name}
+                                  </h2>
+                                </div>
+                              ))}
+                            </div>
+                            <div className="w-1/2 relative items-start mt-8">
+                              {tab.collaborators.map((collaborator, index) => (
+                                <div
+                                  key={index}
+                                  style={{
+                                    left: index * 24,
+                                  }}
+                                  className="absolute"
+                                >
+                                  <Image
+                                    src={collaborator.imageSrc}
+                                    alt="project people"
+                                    width={35}
+                                    height={35}
+                                    className="rounded-full"
+                                  />
+                                </div>
+                              ))}
+                            </div>
+                            <div className="w-full pl-28 items-end pt-2">
+                              <h2 className="text-normal font-medium text-black">
+                                {tab.collaborationText}
+                              </h2>
+                            </div>
+                          </div>
+                        </div>
+                      )}
+                      {!tab.imageSrc && <div>{tab.title}</div>}
+                    </TabsContent>
+                  ))}
+                </div>
+              </Tabs>
+              <div className="mr-24">
+                <ComboboxDemo />
+              </div>
+            </div>
+            <div className="w-full justify-end inline-flex pr-24 pt-4 ">
+            <Button
+              variant="default"
+              size="sm"
+              className="w-auto flex gap-2 h-auto py-3 px-4 cursor-pointer mt-8 rounded-xl"
+            >
+              <FiPlus size={20} />
 
+              <h1 className="font-light text-normal ">
+                Add Project
+              </h1>
+            </Button>
             </div>
 
+          </div>
         </div>
-
+      </div>
     </div>
-  </div>
- );
+  );
 };
-
-
 
 export default ProfilePage;
