ded998e8a24d660d0d87d0659d46632ef25849fb
Fadzaa <fadza20@gmail.com>
2024-07-11 20:56:38 +0700

    feat: [FA] add navbar and navigation route

--- src/app/components/navbar.tsx
+++ src/app/components/navbar.tsx
@@ -1,13 +1,66 @@
 import Image from "next/image";
+import Link from "next/link";
+import { IoIosNotificationsOutline } from "react-icons/io";
+import { FaPlus } from "react-icons/fa6";
+
+interface NavbarProps {
+    href: string;
+    title: string;
+}
+
+const dataNavbar: NavbarProps[] = [
+    {
+        href: "/",
+        title: "Home"
+    },
+    {
+        href: "/project",
+        title: "Project"
+    },
+    {
+        href: "/messages",
+        title: "Messages"
+    }
+]
+
+
 
 export default function Navbar() {
     return (
-        <div className="w-full bg-primary flex justify-between px-[120px] py-[20px]">
+        <div className="w-full flex justify-between items-center px-[120px] py-[20px] border border-b-[#C1C1C1] fixed">
             <Image
                 src="/assets/svg/logo-horizontal.svg"
                 alt="Logo Horizontal"
-                width={200}
-                height={50} />
+                width={135}
+                height={60} />
+
+            <ul className="flex gap-[80px]">
+                {dataNavbar.map((item, index) => (
+                    <Link href={item.href} key={index}>
+                        <li className="font-medium text-lg text-primary">
+                            {item.title}
+                        </li>
+                    </Link>
+                ))}
+
+            </ul>
+
+            <div className="flex items-center gap-[35px]">
+                <IoIosNotificationsOutline className="text-primary" size={32}/>
+
+                <Link href="/profile">
+                    <Image src="/assets/image/user-avatar.png" alt="User Avatar" width={40} height={40} className="rounded-full" />
+                </Link>
+
+                <Link href="/project-collaborate">
+                    <button className="bg-primary flex items-center gap-3 px-[20px] py-[10px] rounded-[8px]">
+                        <FaPlus className="text-white" size={24}/>
+
+                        <h3 className="text-white text-xl">Collaborate</h3>
+
+                    </button>
+                </Link>
+            </div>
 
 
         </div>
