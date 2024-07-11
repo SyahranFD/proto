14bd9a1b12c2488caea537a7ae3b87acae2d23e0
hao47 <daffarobani551@gmail.com>
2024-07-11 23:40:36 +0700

    [DF] setup-actions-api

---
+++ src/app/(page)/(initial-pages)/login/components/form-layout.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import {Input} from "@/app/components/ui/input";
+import {Button} from "@/app/components/ui/button";
+
+const FormLayoutRegister:React.FC = () => {
+ return (
+     <form className={'w-full flex flex-col gap-6'}>
+         <div className={'w-full flex flex-col gap-3'}>
+             <label className={'font-semibold text-lg'}>Email</label>
+             <Input placeholder={"Enter your email"} className={'h-14 rounded-lg'}/>
+         </div>
+         <div className={'w-full flex flex-col gap-3'}>
+             <label className={'font-semibold text-lg'}>Password</label>
+             <Input placeholder={"Enter your password"} className={'h-14 rounded-lg'}/>
+         </div>
+
+
+         <Button className={'mt-4'}>Sign in</Button>
+     </form>
+ );
+};
+
+
+export default FormLayoutRegister;
--- src/app/lib/services/instance/instance.ts
+++ src/app/lib/services/instance/instance.ts
@@ -8,3 +8,6 @@ const instance = axios.create({
         "Accept": "application/json",
     }
 });
+
+
+export default instance
